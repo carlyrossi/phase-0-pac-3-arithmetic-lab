a1f7509d0804ab25cc8ce9806c4f675cba8cda2b
Carly <+>
2021-12-04 12:23:46 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b) {
+ let ans = a+b
+ return ans
+}
+function subtract(a,b) {
+    let ans = a-b
+    return ans
+}
+function multiply(a,b) {
+    let ans = a*b
+    return ans
+}
+function divide(a,b) {
+    let ans = a/b
+    return ans
+}
+function increment(n) {
+    n+=1
+    return n
+}
+function decrement(n) {
+    n-=1
+    return n
+}
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n, 10)
+}
